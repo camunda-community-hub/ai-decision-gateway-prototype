acfb727c328f8e195c0bd441749b1ef9e3505dc0
Sebastian Stamm <sebastian.stamm@camunda.com>
2024-04-23 11:19:41 +0200

    connect to local ES and construct single list of all relevant events
    - group list of events by process instances
    - for every process instance, construct a list of variables before and after flownode

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client } = require('@elastic/elasticsearch');
+
+const client = new Client({
+    node: 'http://localhost:9200'
+});
+
+(async () => {
+    const variableResult = await client.search({
+        index: 'zeebe-record-variable',
+        size: 10000
+    });
+    const historyResult = await client.search({
+        index: 'zeebe-record-process-instance',
+        size: 10000
+    });
+
+    let aggregate = variableResult.hits.hits.reduce((acc, curr) => {
+        if (!acc[curr._source.partitionId]) {
+            acc[curr._source.partitionId] = []
+        }
+
+        acc[curr._source.partitionId].push(curr);
+
+        return acc;
+    }, {});
+    aggregate = historyResult.hits.hits.reduce((acc, curr) => {
+        if (!acc[curr._source.partitionId]) {
+            acc[curr._source.partitionId] = []
+        }
+
+        acc[curr._source.partitionId].push(curr);
+
+        return acc;
+    }, aggregate);
+
+
+    Object.values(aggregate).forEach(list => {
+        list.sort((a, b) => a._source.position - b._source.position);
+    });
+
+
+    console.log(aggregate);
+})();
